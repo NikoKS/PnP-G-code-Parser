4648993b68142f099eefe48af1158380f652c174
NikolasSetiawan <nise7689@colorado.edu>
2018-10-18 14:49:49 -0600

    updated for multi tray

--- PnP_GP2.py
+++ PnP_GP2.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 from sys import platform
-from PyQt5.QtWidgets import (QApplication, QTextEdit, QLabel,
+from PyQt5.QtWidgets import (QApplication, QTextEdit, QLabel, QTabWidget, QComboBox,
                              QVBoxLayout, QHBoxLayout, QWidget, QLineEdit, QPushButton,
                              QFormLayout, QRadioButton, QTableWidget, QFrame,
                              QFileDialog, QMessageBox, QTableWidgetItem)
@@ -34,11 +34,11 @@ G4 P500                 ;Delay half second\n\
 G1 Z[place_Z]           ;Place item\n\
 G4 P500                 ;Delay\n\
 M722 I1 T[PnP_head]     ;Push Item\n\
-G4 P3000                 ;Delay\n\
+G4 P3000                ;Delay\n\
 M107 T[PnP_head]        ;Turn off vacuum\n\
 G4 P500                 ;Delay\n\
 M721 I1 T[PnP_head]     ;unprime current head\n\
-G4 P2000                 ;delay 1s\n"
+G4 P2000                ;delay 1s\n"
 
 Pick_Gcode_S = "\n\
 G0 Z[height]            ;lower bed 1cm\n\
@@ -54,7 +54,7 @@ G4 P500                 ;Delay half second\n\
 G1 Z[place_Z]           ;Place item\n\
 G4 P500                 ;Delay\n\
 M107 T[PnP_head]        ;Turn off vacuum\n\
-G4 P2000                 ;delay 1s\n"
+G4 P2000                ;delay 1s\n"
 
 keyword = ';Do PnP Stuff'
 
@@ -70,20 +70,10 @@ class window(QWidget):
 
         label1 = QLabel('PnP Tray Setting')
         label1.setFont(subTFont)
-        label2 = QLabel('First X Tray Point')
-        label3 = QLabel('Head X Offset')
-        label4 = QLabel('X Tray Offset')
-        label5 = QLabel('Y Tray Offset')
-        label6 = QLabel('Head Y Offset')
-        label7 = QLabel('First Y Tray Point')
-        label8 = QLabel('Tray Item Height')
-        label9 = QLabel('Tray X Dim')
-        label10= QLabel('Tray Y Dim')
         label11= QLabel('G-Code Viewer')
         label11.setFont(subTFont)
-        label12= QLabel('Main Head pos')
-        label13= QLabel('PnP Head pos')
 
+        self.Line0 = QLineEdit()    #Number of Trays
         self.Line1 = QLineEdit()    #first X point
         self.Line2 = QLineEdit()    #X Tray offset
         self.Line3 = QLineEdit()    #Y Tray offset
@@ -93,13 +83,13 @@ class window(QWidget):
         self.Line7 = QLineEdit()    #Tray X Dimension
         self.Line8 = QLineEdit()    #Tray Y Dimension
         self.Line9 = QLineEdit()    #Tray Item Height
-        self.FindString = QLineEdit()
+        self.FindString = QLineEdit()   #Search keyword
         self.Line10= QLineEdit()    #Main Head
         self.Line11= QLineEdit()    #PnP Head
 
-        self.But1 = QPushButton('Save')
+        self.But1 = QPushButton('Save Settings')
         self.But1.clicked.connect(self.push1)
-        self.But2 = QPushButton('Load')
+        self.But2 = QPushButton('Load Settings')
         self.But2.clicked.connect(self.push2)
         self.But7 = QPushButton('Load G-Code')
         self.But7.clicked.connect(self.push7)
@@ -113,6 +103,10 @@ class window(QWidget):
         self.But11.clicked.connect(self.push11)
         self.But12= QPushButton('Help')
         self.But12.clicked.connect(self.push12)
+        self.ButTray = QPushButton('Add Extra Tray')
+        self.ButTray.clicked.connect(self.AddTray)
+        self.ButReset = QPushButton('Reset')
+        self.ButReset.clicked.connect(self.Reset)
 
         self.Rad1 = QRadioButton('Standard')
         self.Rad1.setChecked(1)
@@ -121,50 +115,60 @@ class window(QWidget):
         self.Text1 = QTextEdit()
         self.Cpos = 0
 
+        self.tabs = QTabWidget()
+        tab1 = QWidget()
+        self.tabs.addTab(tab1, 'Tray 1')
+        self.Line = []
+        self.Etab = {}
+
         #Window Layout
+        hbox0 = QHBoxLayout()
+        hbox0.addWidget(QLabel('Number of Trays'))
+        hbox0.addWidget(self.Line0)
+        hbox0.addWidget(self.ButTray)
+        hbox0.addWidget(self.ButReset)
+        hbox0.addStretch()
+
         fbox1 = QFormLayout()
-        fbox1.addRow(label2,self.Line1)
-        fbox1.addRow(label4,self.Line2)
-        fbox1.addRow(label5,self.Line3)
-        fbox1.addRow(label12,self.Line10)
-        fbox1.addRow(label9,self.Line7)
-        fbox1.addRow(label10,self.Line8)
+        fbox1.addRow('Initial X Tray', self.Line1)
+        fbox1.addRow('X Tray Offset', self.Line2)
+        fbox1.addRow('X Tray Dimension', self.Line7)
 
         fbox2 = QFormLayout()
-        fbox2.addRow(label7,self.Line6)
-        fbox2.addRow(label3,self.Line4)
-        fbox2.addRow(label6,self.Line5)
-        fbox2.addRow(label13,self.Line11)
-        fbox2.addRow(label8,self.Line9)
+        fbox2.addRow('Initial Y Tray', self.Line6)
+        fbox2.addRow('Y Tray Offset', self.Line3)
+        fbox2.addRow('Y Tray Dimension', self.Line8)
 
         hbox1 = QHBoxLayout()
-        hbox1.addWidget(self.But1)
-        hbox1.addWidget(self.But2)
+        hbox1.addLayout(fbox1)
+        hbox1.addLayout(fbox2)
 
-        vbox1 = QVBoxLayout()
-        vbox1.addLayout(fbox2)
-        vbox1.addLayout(hbox1)
-        vbox1.addStretch()
+        fbox3 = QFormLayout()
+        fbox3.addRow('Head X Offset',self.Line4)
+        fbox3.addRow('Main Head Pos',self.Line10)
+        fbox3.addRow('Tray Item Height',self.Line9)
 
-        hbox2 = QHBoxLayout()
-        hbox2.addLayout(fbox1)
-        hbox2.addLayout(vbox1)
-        hbox2.addStretch()
-
-        vbox2 = QVBoxLayout()
-        vbox2.addWidget(label1)
-        vbox2.addLayout(hbox2)
+        fbox4 = QFormLayout()
+        fbox4.addRow('Head Y Offset',self.Line5)
+        fbox4.addRow('PNP Hed Pos',self.Line11)
 
         hbox6 = QHBoxLayout()
         hbox6.addWidget(self.Rad1)
         hbox6.addWidget(self.Rad2)
 
-        vbox8 = QVBoxLayout()
-        vbox8.addLayout(vbox2)
-        vbox8.addLayout(hbox6)
-        vbox8.addStretch()
-        vbox8.addWidget(self.But12)
-        vbox8.addStretch()
+        vbox1 = QVBoxLayout()
+        vbox1.addLayout(fbox4)
+        vbox1.addLayout(hbox6)
+
+        hbox2 = QHBoxLayout()
+        hbox2.addLayout(fbox3)
+        hbox2.addLayout(vbox1)
+
+        tab1.setLayout(hbox1)
+
+        hbox3 = QHBoxLayout()
+        hbox3.addWidget(self.But1)
+        hbox3.addWidget(self.But2)
 
         hbox4 = QHBoxLayout()
         hbox4.addWidget(self.But11)
@@ -178,19 +182,31 @@ class window(QWidget):
         hbox5.addWidget(self.But9)
         hbox5.addWidget(self.But10)
 
+        vbox2 = QVBoxLayout()
+        vbox2.addWidget(label1)
+        vbox2.addLayout(hbox0)
+        vbox2.addWidget(self.tabs)
+        vbox2.addStretch()
+        vbox2.addLayout(hbox2)
+        vbox2.addLayout(hbox3)
+        vbox2.addStretch()
+        vbox2.addWidget(self.But12)
+        vbox2.addStretch()
+
         vbox9 = QVBoxLayout()
         vbox9.addLayout(hbox5)
         vbox9.addWidget(self.Text1)
         vbox9.addLayout(hbox4)
 
         FinalBox = QHBoxLayout()
-        FinalBox.addLayout(vbox8)
+        FinalBox.addLayout(vbox2)
         FinalBox.addLayout(vbox9)
 
         #vbox 7 = add point, vbox 2 = setting
         self.setLayout(FinalBox)
+
         #Window Functions
-        self.setGeometry(300,300,1500,600)
+        self.setGeometry(300,300,1600,600)
         self.setWindowTitle('Pick and Place Gcode Parser')
         self.show()
 
@@ -206,18 +222,26 @@ class window(QWidget):
         TC = Tool_Change_Gcode.replace('[current_head]', self.Line10.text())
         TC = TC.replace('[height]', str(float(self.height)+10))
         cursor.insertText(TC)
+
         #Insert PNP Operation G-Code
         for item in placePos:
             item[0] = round(np.mean(item[0]), 3) + float(self.Line4.text())
             item[1] = round(np.mean(item[1]), 3) + float(self.Line5.text())
             print(item)
-            xdim = float(self.Line7.text())
-            ydim = float(self.Line8.text())
-            if self.Rad1.isChecked():
-                PG = Pick_Gcode_S.replace('[pick_X]', str(self.dispenser[0]))
+            if not self.Line or item[2] > 6:
+                disp = 0            #Tray/dispenser number
+                xdim = float(self.Line7.text())
+                ydim = float(self.Line8.text())
             else:
-                PG = Pick_Gcode.replace('[pick_X]', str(self.dispenser[0]))
-            PG = PG.replace('[pick_Y]', str(self.dispenser[1]))
+                disp = item[2] - 3
+                xdim = float(self.Line[disp-1][2].text())
+                ydim = float(self.Line[disp-1][5].text())
+
+            if self.Rad1.isChecked():
+                PG = Pick_Gcode_S.replace('[pick_X]', str(self.dispenser[disp][0]))
+            else:
+                PG = Pick_Gcode.replace('[pick_X]', str(self.dispenser[disp][0]))
+            PG = PG.replace('[pick_Y]', str(self.dispenser[disp][1]))
             PG = PG.replace('[place_X]', str(item[0]))
             PG = PG.replace('[place_Y]', str(item[1]))
             PG = PG.replace('[height]', str(float(self.height)+10))
@@ -225,30 +249,100 @@ class window(QWidget):
             PG = PG.replace('[place_Z]', self.height)
             PG = PG.replace('[PnP_head]', self.Line11.text())
             cursor.insertText(PG)
-            if self.change != xdim:
-                self.dispenser[0] += float(self.Line2.text())
-                self.change += 1
+            if self.change[disp] != xdim:
+                if disp == 0:
+                    self.dispenser[disp][0] += float(self.Line2.text())
+                    self.change[disp] += 1
+                else:
+                    self.dispenser[disp][0] += float(self.Line[disp-1][1])
+                    self.change[disp] += 1
             else:
-                self.dispenser[0] = float(self.Line1.text()) - self.Xorigin
-                self.dispenser[1] += float(self.Line3.text())
-                self.change = 1
+                if disp == 0:
+                    self.dispenser[disp][0] = float(self.Line1.text()) - self.Xorigin
+                    self.dispenser[disp][1] += float(self.Line3.text())
+                    self.change[disp] = 1
+                else:
+                    self.dispenser[disp][0] = float(self.Line[disp-1][2]) - self.Xorigin
+                    self.dispenseri[disp][1] += float(self.Line[disp][4])
+                    self.changei[disp] = 1
+
         TC2 = Tool_Change_Gcode2.replace('[current_head]', self.Line10.text())
         cursor.insertText(TC2)
         print('finnished adding G-code')
 
-    def push1(self):  #Save Settings
-        filename = QFileDialog.getSaveFileName(self, 'Save Settings')
-        if not filename[0]:
-            QMessageBox.warning(self,"Warning","Filename Empty!")
+    def Reset(self):
+        Lines = [self.Line1,self.Line2,self.Line3,self.Line4,self.Line5,self.Line6,
+                self.Line7,self.Line8,self.Line9,self.Line10,self.Line11]
+        for item in Lines:
+            item.setText('')
+        self.Line0.setText('')
+        self.updatesEnabled = False
+        for item in self.Etab:
+            self.tabs.removeTab(1)
+        self.Line = []
+        self.Etab = {}
+        self.updatesEnabled = True
+
+    def AddTray(self):           #Add extra tray for Pick and Place
+        self.Trays = self.Line0.text()
+        if not self.isfloat(self.Trays):
+            QMessageBox.warning(self,"Warning","Not a Number!")
+        elif int(self.Trays) == 1:
+            pass
+        elif int(self.Trays) > 6:
+            QMessageBox.warning(self,"Warning","Too Many Trays!")
         else:
-            with open(filename[0],'w') as f:
-                Lines = [self.Line1.text(),self.Line2.text(),self.Line3.text(),self.Line4.text(),
-                        self.Line5.text(),self.Line6.text(),self.Line7.text(),self.Line8.text(),
-                        self.Line9.text(),self.Line10.text(),self.Line11.text()]
-                if not all([self.isfloat(i) for i in Lines]):
-                    QMessageBox.information(self,"Warning","Not a Number Detected")
-                else:
+            self.Trays = int(self.Trays)
+            self.updatesEnabled = False
+            for i in range(len(self.Line)+2,self.Trays+1):
+                self.Etab[i] = QWidget()
+                self.tabs.addTab(self.Etab[i], 'Tray {}'.format(i))
+                self.Line.append([])
+                for j in range(6):
+                    self.Line[i-2].append(QLineEdit())
+                hbox = QHBoxLayout()
+                fbox = [QFormLayout(), QFormLayout()]
+                fbox[0].addRow('Initial X Tray',self.Line[i-2][0])
+                fbox[0].addRow('X Tray Offset',self.Line[i-2][1])
+                fbox[0].addRow('X Tray Dimension',self.Line[i-2][2])
+                fbox[1].addRow('Initial Y Tray',self.Line[i-2][3])
+                fbox[1].addRow('Y Tray Offset',self.Line[i-2][4])
+                fbox[1].addRow('Y Tray Dimension',self.Line[i-2][5])
+                hbox.addLayout(fbox[0])
+                hbox.addLayout(fbox[1])
+                self.Etab[i].setLayout(hbox)
+            self.updatesEnabled = True
+
+    def push1(self):  #Save Settings
+        Lines = [self.Line1.text(),self.Line2.text(),self.Line3.text(),self.Line4.text(),
+                self.Line5.text(),self.Line6.text(),self.Line7.text(),self.Line8.text(),
+                self.Line9.text(),self.Line10.text(),self.Line11.text()]
+        Line = []
+        if not self.Line:
+            check = 1
+        else:
+            Line = [[x.text() for x in y] for y in self.Line]
+            check = all([all([self.isfloat(x) for x in y]) for y in Line])
+        if not all([self.isfloat(i) for i in Lines]) or not check:
+            QMessageBox.information(self,"Warning","Not a Number Detected in Settings")
+        else:
+            filename = QFileDialog.getSaveFileName(self, 'Save Settings')
+            if not filename[0]:
+                QMessageBox.warning(self,"Warning","Filename Empty!")
+            else:
+                with open(filename[0],'w') as f:
                     file_text = ','.join(Lines)
+                    num = ''
+                    for item in Line:
+                        file_text = file_text + ',' +  ','.join(item)
+                    try:
+                        self.Trays
+                    except:
+                        num = '1,'
+                    else:
+                        num = str(self.Trays) + ','
+                    file_text = num + file_text
+                    print(file_text)
                     f.write(file_text)
 
     def push2(self):    #Load Settings
@@ -261,8 +355,17 @@ class window(QWidget):
                         self.Line7,self.Line8,self.Line9,self.Line10,self.Line11]
                 f_string = f.read()
                 item = f_string.split(',')
-                for i in range(len(item)):
-                    Lines[i].setText(item[i])
+                num = int(item[0])
+                for i in range(1,12):
+                    Lines[i-1].setText(item[i])
+                if num > 1:
+                    self.Line0.setText(item[0])
+                    self.AddTray()
+                    cnt = 0
+                    for x in self.Line:
+                        for y in x:
+                            y.setText(item[12 + cnt])
+                            cnt += 1
 
     def push8(self): #Save GCode
         filename = QFileDialog.getSaveFileName(
@@ -329,16 +432,23 @@ class window(QWidget):
         Lines = [self.Line1.text(),self.Line2.text(),self.Line3.text(),self.Line4.text(),
                 self.Line5.text(),self.Line6.text(),self.Line7.text(),self.Line8.text(),
                 self.Line9.text(),self.Line10.text(),self.Line11.text()]    #User input lines
-
+        if not self.Line:
+            check = 1
+        else:
+            check = all([all([self.isfloat(y.text()) for y in x]) for x in self.Line])
         cursor1 = document.find('T3 ; change extruder', cursor1)
         if cursor1.position() == -1:    #Check if there's a result for the keyword find
             QMessageBox.information(self,"Info","No Pick and Place operation found on the GCode",QMessageBox.Ok)
-        elif not all([self.isfloat(i) for i in Lines]):     #Check if all the input propper numbers
+        elif not all([self.isfloat(i) for i in Lines]) or not check:     #Check if all the input propper numbers
             QMessageBox.information(self,"Warning","Not a Number Detected in user input!")
         else:
-            self.dispenser = [float(self.Line1.text())-self.Xorigin,    #dispenser location start
-                              float(self.Line6.text())-self.Yorigin]
-            self.change = 1
+            #Set up Tray location(s)
+            self.dispenser = [[float(self.Line1.text())-self.Xorigin,    #dispenser location start
+                              float(self.Line6.text())-self.Yorigin]]
+            self.change = [1]     #For keeping track change change in tray row
+            for item in self.Line:
+                self.dispenser.append([float(item[0]), float(item[3])])
+                self.change.append(1)
 
             ended = 0 #flag for determining if the parser should keep searching for next pick and place operation block
             while not ended:
@@ -441,7 +551,7 @@ class window(QWidget):
 dfont = QFont("Arial",18)
 app = QApplication([])
 app.setFont(dfont)
-window1=window()
+window1 = window()
 app.exec_()
 
 
